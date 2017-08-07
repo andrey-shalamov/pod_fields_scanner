6d9ec43bb2cff0a65500b45ee1ff2f4ab9f3e076
andrey <andrey.shalamov@gmail.com>
2017-08-07 18:00:32 +0300

    code's improvement

--- pod_fields_count_detector.h
+++ pod_fields_count_detector.h
@@ -9,20 +9,21 @@ struct pod_fields_count_detector
 {
 	static constexpr size_t detect() noexcept
 	{
-		return std::get<size_t>(detect_impl(std::make_index_sequence<sizeof(PodType)>{}));
+		return detect_impl(std::make_index_sequence<sizeof(PodType)>{});
 	}
 
 private:
 	template<size_t... Is>
-	static constexpr auto detect_impl(std::index_sequence<Is...>) noexcept
+	static constexpr size_t detect_impl(std::index_sequence<Is...>) noexcept
 	{
 		return detect_impl(std::make_index_sequence<sizeof...(Is)-1>{});
 	}
 
 	template<size_t I0, size_t... Is>
-	static constexpr auto detect_impl(std::index_sequence<I0, Is...>) noexcept ->
-		std::tuple < size_t, decltype(PodType{ as_any_type<I0>{}, as_any_type<Is>{}... }) >
+	static constexpr
+		std::enable_if_t<std::is_same<PodType, decltype(PodType{ as_any_type<I0>{}, as_any_type<Is>{}... })>::value, size_t>
+		detect_impl(std::index_sequence<I0, Is...>) noexcept
 	{
-		return { 1 + sizeof...(Is), {} };
+		return 1 + sizeof...(Is);
 	}
 };
